9b713b509a577a1d11f0d453ebbf57dcb99e7f13
ripterdust <bryantello2010@hotmail.com>
2022-04-06 12:47:18 -0600

    Added the routers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,16 @@
 import express from 'express';
+import { authRouter } from './router/auth.router';
+import { noteRouter } from './router/note.router';
 
 // Initialization
 export const app = express();
 
 // Configuration
 app.set('port', process.env.PORT || 3000);
+
+// routes
+app
+    // Notes
+    .use('/', noteRouter)
+    // Auth
+    .use('/auth', authRouter);
