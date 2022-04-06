a3964dea67228ad7ef13f5ee8c2b79d8117b50dd
ripterdust <bryantello2010@hotmail.com>
2022-04-06 13:03:03 -0600

    Exported the class

--- src/database/sqlite.ts
+++ src/database/sqlite.ts
@@ -1,6 +1,10 @@
 const sqlite3 = require('sqlite3').verbose();
 
 // @ts-ignore
-const db = new sqlite3().Database('./note', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) return console.log(err);
-});
+export const db = new sqlite3().Database(
+    './note',
+    sqlite3.OPEN_READWRITE,
+    (err) => {
+        if (err) return console.log(err);
+    }
+);
