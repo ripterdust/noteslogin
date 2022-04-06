5ef2aec5df28e9ebe243a33f04ae52d495f18fd4
ripterdust <bryantello2010@hotmail.com>
2022-04-06 13:03:16 -0600

    Fixed ts

--- src/database/sqlite.ts
+++ src/database/sqlite.ts
@@ -1,9 +1,9 @@
 const sqlite3 = require('sqlite3').verbose();
 
-// @ts-ignore
 export const db = new sqlite3().Database(
     './note',
     sqlite3.OPEN_READWRITE,
+    // @ts-ignore
     (err) => {
         if (err) return console.log(err);
     }
