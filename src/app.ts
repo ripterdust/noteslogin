77498611c600e6ebec23721975074808acb6c087
ripterdust <bryantello2010@hotmail.com>
2022-04-06 13:08:15 -0600

    COnfigured the views folder

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import { authRouter } from './router/auth.router';
 import { noteRouter } from './router/note.router';
+import { join } from 'path';
 
 // Initialization
 export const app = express();
 
 // Configuration
 app.set('port', process.env.PORT || 3000);
+app.set('views', join(__dirname, 'views'));
 
 // routes
 app
