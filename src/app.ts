f391382eeddb776d3b3ecf5d4b372e38487614fb
ripterdust <bryantello2010@hotmail.com>
2022-04-06 13:24:14 -0600

    Added the partials dirs

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,18 @@ import express from 'express';
 import { authRouter } from './router/auth.router';
 import { noteRouter } from './router/note.router';
 import { join } from 'path';
+import { create } from 'express-handlebars';
 
 // Initialization
 export const app = express();
-
+const hbs = create({
+    partialsDir: ['/partials/notes', '/partials/auth'],
+});
 // Configuration
 app.set('port', process.env.PORT || 3000);
 app.set('views', join(__dirname, 'views'));
-
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
 // routes
 app
     // Notes
